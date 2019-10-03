9b55cf2f9789f0e43fd09951aabb0de05ac3ddcd
AJC257 <48887216+AJC257@users.noreply.github.com>
2019-10-03 14:07:07 +0100

    Added SpaceShip Table
    subject to change

--- Database/Users.sql
+++ Database/Users.sql
@@ -6,4 +6,13 @@ CREATE TABLE Users(
   password VARCHAR(30)
 );
 
+CREATE TABLE SpaceShip(
+  shipName VARCHAR(15) NOT NULL PRIMARY KEY,
+  captain VARCHAR(15),
+  mainColour VARCHAR(10),
+  secondColour VARCHAR(10),
+  FOREIGN KEY (captain) REFERENCES Users(username)
+);
+
 INSERT INTO Users VALUES("AJC257", "Coburn", "Andrew", "ajc15@hw.ac.uk", "example1234");
+INSERT INTO SpaceShip VALUES("U.S.S Example", "AJC257", "green", "purple");
