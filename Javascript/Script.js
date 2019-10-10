182f6a706ef1aadcd283d16ef9d13dc7ccf0f8da
AJC257 <48887216+AJC257@users.noreply.github.com>
2019-10-10 14:45:49 +0100

    Added sidebar stuff to js file

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -17,3 +17,14 @@ var myGameArea = {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 } //to do
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
