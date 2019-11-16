86667c6dd3a36272a70e3512b1f7a207b3c785fd
Nabil Ghayyda <ng47@hw.ac.uk>
2019-11-16 21:32:06 +0000

    fixed it up a bit now it starts with the login screen

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 var serv = require('http').Server(app);
 
 app.get('/' ,function(req,res) {
-  res.sendFile(__dirname + '/game.html');
+  res.sendFile(__dirname + '/index.html');
+
 });
+app.use('/',express.static(__dirname + '/'));
 app.use('/javascript',express.static(__dirname + '/javascript'));
 app.use('/Style',express.static(__dirname + '/Style'));
-app.use('/',express.static(__dirname + '/index.html'));
-app.use('/',express.static(__dirname + '/customise.html'));
 
 serv.listen(2000);
