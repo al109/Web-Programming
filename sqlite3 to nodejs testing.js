da02a40beaf3d8e914e0676d0c023eca71775046
AJC257 <48887216+AJC257@users.noreply.github.com>
2019-11-19 16:26:30 +0000

    Added more queryies to test file

--- sqlite3 to nodejs testing.js
+++ sqlite3 to nodejs testing.js
@@ -12,7 +12,9 @@ let databasetest = new sqlite3.Database('/Database/Users.sql', (err) => {
 });
 
 
-//should only return first row of Users database
+
+
+//should only return first row of Users table
 let sqlquery = 'Select * FROM Users';
 
 databasetest.get(sqlquery, (err, row)) => {
@@ -23,7 +25,7 @@ databasetest.get(sqlquery, (err, row)) => {
     ? console.log(row.username, row.surname, row.firstName, row.email, row.password);
 }
 
-
+// should return every row from the SpaceShip table
 let sqlquery2 = `SELECT * FROM SpaceShip`;
 
 db.each(sqlquery2, (err, row) => {
@@ -32,6 +34,57 @@ db.each(sqlquery2, (err, row) => {
   }
   console.log(`${row.shipName} ${row.captain} ${row.mainColour} ${row.secondColour}`);
 });
+
+
+
+
+//insert a new user into the Users table
+let newUser = ['insert', 'insertsname', 'insertfname', 'insert@hw.ac.uk', 'insert'];
+
+// construct the insert statement with multiple placeholders
+// based on the number of rows
+let placeholders = databasetest.map((newUser) => '(?)').join(',');
+let sqlinsert = 'INSERT INTO Users(name) VALUES ' + placeholders;
+
+// output the INSERT statement
+console.log(sqlinsert);
+
+db.run(sqlinsert, newUser, function(err) {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log(`Rows inserted ${this.changes}`);
+});
+
+
+
+//Update a user in users table
+let updatedata = ['752CJA', 'AJC257'];
+let sqlupdate = `UPDATE Users
+            SET username = ?
+            WHERE username = ?`;
+
+db.run(sqlupdate, updatedata, function(err) {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log(`Row(s) updated: ${this.changes}`);
+
+});
+
+
+
+//delete a user from the Users table
+let deleteduser = 'Example';
+// delete a row based on id
+db.run(`DELETE FROM Users WHERE username = ?`, deleteduser, function(err) {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log(`Row(s) deleted ${this.changes}`);
+});
+
+
 
 // close the database connection
 db.close((err) => {
