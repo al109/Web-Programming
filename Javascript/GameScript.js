f672b120549603e4864142393e65067d874429b1
Nabil Ghayyda <ng47@hw.ac.uk>
2019-11-23 22:22:45 +0000

    hopefully this works lol

--- Javascript/GameScript.js
+++ Javascript/GameScript.js
@@ -15,79 +15,48 @@ socket.emit('start',{
   name: "Yooo, it started lads"
 
 });
-
 socket.on('newPositions',function(data){
   ctx.clearRect(0,0,canvas.width,canvas.height);
-    for(var i = 0; i < data.length;i++){
-      if (data[i].x > canvas.width -40) {
-      
-      data[i].x = canvas.width - 40;
-      } else if (data[i].x < 40) {
-        data[i].x = 40;
-      }
+    for(var i = 0; i < data.length;i++)
+      rotateAndPaintImage(ctx,image,data[i].rotation*TO_RADIANS,data.player[i].x,data.player[i].y,20,30);
+    for(var i = 0 ; i < data.bullet.length; i++)
+      ctx.fillRect(data.bullet[i].x-5,data.bullet[i].y-5,10,10);
+          });
 
-     if (data[i].y > canvas.height - 40) {
-      data[i].y = canvas.height - 40;
-     } else if (data[i].y < 40) {
-      data[i].y = 40;
-        
-      }
-      rotateAndPaintImage(ctx,image,data[i].rotation*TO_RADIANS,data[i].x,data[i].y,20,30);
-    }
-});
+          document.onkeydown = function(event){
+              if(event.keyCode === 68)    //d
+                  socket.emit('keyPress',{inputId:'right',state:true});
+              else if(event.keyCode === 83)   //s
+                  socket.emit('keyPress',{inputId:'down',state:true});
+              else if(event.keyCode === 65) //a
+                  socket.emit('keyPress',{inputId:'left',state:true});
+              else if(event.keyCode === 87) // w
+                  socket.emit('keyPress',{inputId:'up',state:true});
 
-document.onkeydown = function(event){
-  if(event.keyCode==68)
-    socket.emit('keyPress',{
-      inputId:'right',
-      state:true,
-      rotation:90
-});
-  else if(event.keyCode==83)
-  socket.emit('keyPress',{
-    inputId:'down',
-    state:true,
-    rotation:180
-});
-  else if(event.keyCode==65)
-  socket.emit('keyPress',{
-    inputId:'left',
-    state:true,
-    rotation:270
-});
-  else if(event.keyCode==87)
-  socket.emit('keyPress',{
-    inputId:'up',
-    state:true,
-    rotation:0
-});
-}
+          }
+          document.onkeyup = function(event){
+              if(event.keyCode === 68)    //d
+                  socket.emit('keyPress',{inputId:'right',state:false});
+              else if(event.keyCode === 83)   //s
+                  socket.emit('keyPress',{inputId:'down',state:false});
+              else if(event.keyCode === 65) //a
+                  socket.emit('keyPress',{inputId:'left',state:false});
+              else if(event.keyCode === 87) // w
+                  socket.emit('keyPress',{inputId:'up',state:false});
+          }
 
-document.onkeyup = function(event){
-  if(event.keyCode==68)
-    socket.emit('keyPress',{
-      inputId:'right',
-      state:false,
-      rotation:90
-});
-  else if(event.keyCode==83)
-  socket.emit('keyPress',{
-    inputId:'down',
-    state:false,
-    rotation:180
-});
-  else if(event.keyCode==65)
-  socket.emit('keyPress',{
-    inputId:'left',
-    state:false,
-    rotation:270
-});
-  else if(event.keyCode==87)
-  socket.emit('keyPress',{
-    inputId:'up',
-    state:false,
-    rotation:0
-});
+          document.onmousedown = function(event){
+              socket.emit('keyPress',{inputId:'attack',state:true});
+          }
+          document.onmouseup = function(event){
+              socket.emit('keyPress',{inputId:'attack',state:false});
+          }
+          document.onmousemove = function(event){
+              var x = -250 + event.clientX - 8;
+              var y = -250 + event.clientY - 8;
+              var angle = Math.atan2(y,x) / Math.PI * 180;
+              socket.emit('keyPress',{inputId:'mouseAngle',state:angle});
+
 }
 function rotateAndPaintImage ( context, image, angleInRad , positionX, positionY, axisX, axisY ) {
   context.translate( positionX, positionY );
--- app.js
+++ app.js
@@ -23,102 +23,177 @@ console.log("Server started");
 
 var io = require('socket.io')(serv,{});
 var SOCKET_LIST = {};
-var PLAYER_LIST = {};
+
+var Entity = function(){
+    var self = {
+        x:250,
+        y:250,
+        spdX:0,
+        spdY:0,
+        id:"",
+    }
+    self.update = function(){
+        self.updatePosition();
+    }
+    self.updatePosition = function(){
+        self.x += self.spdX;
+        self.y += self.spdY;
+    }
+    self.getDistance = function(pt){
+        return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2));
+    }
+    return self;
+}
+
+var Player = function(id){
+    var self = Entity();
+    self.id = id;
+    self.number = "" + Math.floor(10 * Math.random());
+    self.pressingRight = false;
+    self.pressingLeft = false;
+    self.pressingUp = false;
+    self.pressingDown = false;
+    self.pressingAttack = false;
+    self.mouseAngle = 0;
+    self.maxSpd = 10;
+
+    var super_update = self.update;
+    self.update = function(){
+        self.updateSpd();
+        super_update();
+
+        if(self.pressingAttack){
+            self.shootBullet(self.mouseAngle);
+        }
+    }
+    self.shootBullet = function(angle){
+        var b = Bullet(self.id,angle);
+        b.x = self.x;
+        b.y = self.y;
+    }
 
 
-var Player = function(id,rotation){
-  var self = {
-    x:250,
-    y:250,
-    id:id,
-    number:"" + Math.floor(10 * Math.random()),
-    pressingRight:false,
-    pressingLeft:false,
-    pressingUp:false,
-    pressingDown:false,
-    rotation:rotation,
-    maxSpd:5,
-  }
-  self.updatePosition = function(){
-    if(self.pressingRight)
-        self.x += self.maxSpd;
-    if(self.pressingLeft)
-      self.x -= self.maxSpd;
-    if(self.pressingUp)
-      self.y -= self.maxSpd;
-    if(self.pressingDown)
-      self.y += self.maxSpd;
-  }
-  return self;
+    self.updateSpd = function(){
+        if(self.pressingRight)
+            self.spdX = self.maxSpd;
+        else if(self.pressingLeft)
+            self.spdX = -self.maxSpd;
+        else
+            self.spdX = 0;
+
+        if(self.pressingUp)
+            self.spdY = -self.maxSpd;
+        else if(self.pressingDown)
+            self.spdY = self.maxSpd;
+        else
+            self.spdY = 0;
+    }
+    Player.list[id] = self;
+    return self;
+}
+Player.list = {};
+Player.onConnect = function(socket){
+    var player = Player(socket.id);
+    socket.on('keyPress',function(data){
+        if(data.inputId === 'left')
+            player.pressingLeft = data.state;
+        else if(data.inputId === 'right')
+            player.pressingRight = data.state;
+        else if(data.inputId === 'up')
+            player.pressingUp = data.state;
+        else if(data.inputId === 'down')
+            player.pressingDown = data.state;
+        else if(data.inputId === 'attack')
+            player.pressingAttack = data.state;
+        else if(data.inputId === 'mouseAngle')
+            player.mouseAngle = data.state;
+    });
+}
+Player.onDisconnect = function(socket){
+    delete Player.list[socket.id];
+}
+Player.update = function(){
+    var pack = [];
+    for(var i in Player.list){
+        var player = Player.list[i];
+        player.update();
+        pack.push({
+            x:player.x,
+            y:player.y,
+            number:player.number
+        });
+    }
+    return pack;
+}
+
+
+var Bullet = function(parent,angle){
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = Math.cos(angle/180*Math.PI) * 10;
+    self.spdY = Math.sin(angle/180*Math.PI) * 10;
+    self.parent = parent;
+    self.timer = 0;
+    self.toRemove = false;
+    var super_update = self.update;
+    self.update = function(){
+        if(self.timer++ > 100)
+            self.toRemove = true;
+        super_update();
+
+        for(var i in Player.list){
+            var p = Player.list[i];
+            if(self.getDistance(p) < 32 && self.parent !== p.id){
+                //handle collision. ex: hp--;
+                self.toRemove = true;
+            }
+        }
+    }
+    Bullet.list[self.id] = self;
+    return self;
+}
+Bullet.list = {};
+
+Bullet.update = function(){
+    var pack = [];
+    for(var i in Bullet.list){
+        var bullet = Bullet.list[i];
+        bullet.update();
+        if(bullet.toRemove)
+            delete Bullet.list[i];
+        else
+            pack.push({
+                x:bullet.x,
+                y:bullet.y,
+            });
+    }
+    return pack;
 }
 
 
 
-io.sockets.on('connection',function(socket){
-  console.log('made socket connection',socket.id)
-
-  socket.on('start',function(data){
-    var player = Player(socket.id,0);
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+    socket.on('start',function(data){
+    socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
-    PLAYER_LIST[socket.id] = player;
-    console.log(data.name);
-    console.log(socket.id);
 
-
+    Player.onConnect(socket);
+  });
     socket.on('disconnect',function(){
-      delete SOCKET_LIST[socket.id];
-      delete PLAYER_LIST[socket.id];
-    });
-
-    socket.on('keyPress',function(data){
-      if(data.inputId === 'left')
-        player.pressingLeft = data.state;
-        player.rotation = data.rotation;
-      if(data.inputId === 'right')
-        player.pressingRight = data.state;
-        player.rotation = data.rotation;
-      if(data.inputId === 'up')
-        player.pressingUp = data.state;
-        player.rotation = data.rotation;
-      if(data.inputId === 'down')
-        player.pressingDown = data.state;
-        player.rotation = data.rotation;
-      if(data.inputId === 'space'){
-        playerBullets[socket.id] += new Bullet(player.rotation, player.x,player.y)
-      }
-
-  });
-
-
-
-  socket.on('username',function(data){
-    con.connect(function() {
-    var sql = 'INSERT INTO Users (username) VALUES ?';
-    var values = [[data.name]];
-    console.log(data.name);
-    con.query(sql,[values],function(err){
-      if (err) throw err;
-      console.log("IT WORKED REEEEE")
-    });
-  });
-  });
+        delete SOCKET_LIST[socket.id];
+        Player.onDisconnect(socket);
 });
 });
 
 setInterval(function(){
-  var pack = [];
-  for(var i in PLAYER_LIST){
-    var player = PLAYER_LIST[i];
-    player.updatePosition();
-    pack.push({
-      x:player.x,
-      y:player.y,
-      rotation:player.rotation,
-     });
-    
+    var pack = {
+        player:Player.update(),
+        bullet:Bullet.update(),
     }
-  for(var i in SOCKET_LIST){
-    var socket = SOCKET_LIST[i];
-    socket.emit('newPositions',pack);
-  }
+
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.emit('newPositions',pack);
+    }
 },1000/25);
